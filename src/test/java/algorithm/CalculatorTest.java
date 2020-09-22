6eaad8262a1d3b98e726f29ea4abced7cffe7df7
adam <adam@wearedragonfly.co>
2020-09-22 17:35:30 +0200

    mvn -X clean jacoco:prepare-agent verify site

---
+++ src/test/java/algorithm/CalculatorTest.java
@@ -0,0 +1,11 @@
+package algorithm;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+	
+	@Test(timeout = 4000)
+	public void test_Calculator() {
+		Calculator GUI = new Calculator();
+	}
+}
